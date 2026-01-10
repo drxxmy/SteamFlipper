d407bf1b2d6cbb580740c043b24d8f0aaed8abcb
Vladislav <118668125+drxxmy@users.noreply.github.com>
2026-01-10 10:14:53 +0500

    feat(env): add new fine-tuning options

--- src/config/env.py
+++ src/config/env.py
@@ -15,3 +15,9 @@ STEAM_FEE = float(os.getenv("STEAM_FEE", "0.15"))  # ~15%
 MIN_VOLUME = int(os.getenv("MIN_VOLUME", "20"))
 MIN_ROI = float(os.getenv("MIN_ROI", "0.03"))  # 3%
 MIN_PROFIT = float(os.getenv("MIN_PROFIT", "5.0"))  # RUB
+
+RISK_HIGH_SPREAD = float(os.getenv("RISK_HIGH_SPREAD", 0.40))
+RISK_MEDIUM_SPREAD = float(os.getenv("RISK_MEDIUM_SPREAD", 0.25))
+
+RISK_HIGH_MIN_VOLUME = int(os.getenv("RISK_HIGH_MIN_VOLUME", 50))
+RISK_MEDIUM_MIN_VOLUME = int(os.getenv("RISK_MEDIUM_MIN_VOLUME", 150))
