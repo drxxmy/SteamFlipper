7ec7e132dc7a1b7cecea8cd28c54c729b3d74690
Vladislav <118668125+drxxmy@users.noreply.github.com>
2026-01-13 20:25:25 +0500

    feat(api): add post /watchlist endpoint that scans added item

--- backend/src/app/api/watchlist.py
+++ backend/src/app/api/watchlist.py
@@ -1,7 +1,11 @@
+import aiosqlite
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
+from core.env import DB_PATH
 from db.database import add_watchlist_item
+from main import scan_item
+from scraper.steam_market import SteamMarketClient
 
 router = APIRouter(prefix="/watchlist", tags=["watchlist"])
 
@@ -16,5 +20,26 @@ async def add_to_watchlist(data: WatchlistAddIn):
         await add_watchlist_item(data.url)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+    return {"status": "ok"}
+
+
+@router.post("/watchlist")
+async def add_watchlist_and_scan(
+    data: WatchlistAddIn,
+):
+    async with aiosqlite.connect(DB_PATH) as db:
+        item = await add_watchlist_item(data.url)
+
+        client = SteamMarketClient(currency=5)
+
+        await scan_item(
+            db=db,
+            client=client,
+            notifier=None,  # optional
+            item=item,
+        )
+
+        await db.commit()
 
     return {"status": "ok"}
